85c28a165ef2d90d670cf048c28367bc2dd2da0b
Стас Балджи <stack767@gmail.com>
2023-01-26 16:33:35 +0300

    Работы,  которые нужно провести:
    - ограничить функционал джосера до минимума;
    - доработать модель юзера и регистрацию;
    - внести правки в сериализатор BookCard(связанная модель);
    - убрать лишние инструменты для создания документации.

--- bookexchange_project/api/views.py
+++ bookexchange_project/api/views.py
@@ -1,36 +1,14 @@
-# Для Стаса: тут тоже все закомментировал. Мало ли пригодится прям тут.
+from rest_framework import viewsets
+
+from books.models import BookCard, Favorites
+from .serializers import BookCardSerializers, FavoritesSerializer
 
 
-# from books.models import BookAuthor, Book, Genre
-from users.models import User
-from rest_framework import viewsets, permissions, filters
-
-# from .serializers import (
-#     BookSerializer,
-#     BookAuthorSerializer,
-#     GenreSerializer
-# )
-from api.permissions import IsAuthorOrReadOnly
+class BookCardViewSet(viewsets.ModelViewSet):
+    queryset = BookCard.objects.all()
+    serializer_class = BookCardSerializers
 
 
-# class BookViewSet(viewsets.ModelViewSet):
-#     queryset = Book.objects.all()
-#     serializer_class = BookSerializer
-#     permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly,]
-#     filter_backends = (filters.SearchFilter,)
-#     search_fields = (
-#         'author__last_name',
-#         'author__first_name',
-#         'title',
-#         'creation_date',
-#         )
-
-
-# class BookAuthorViewSet(viewsets.ReadOnlyModelViewSet):
-#     queryset = BookAuthor.objects.all()
-#     serializer_class = BookAuthorSerializer
-
-
-# class GenreViewSet(viewsets.ReadOnlyModelViewSet):
-#     queryset = Genre.objects.all()
-#     serializer_class = GenreSerializer
+class FavoritesViewSet(viewsets.ModelViewSet):
+    queryset = Favorites.objects.all()
+    serializer_class = FavoritesSerializer
