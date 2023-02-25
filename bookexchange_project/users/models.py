e83ed2c3c766447f5f975ed9c7019dc9423864e9
Stanislav Chuprinskiy <stas.chuprinskiy@mail.ru>
2023-02-25 03:07:07 +0300

    Изменил модель пользователя
    - Добавил менеджер для сохранения пользовательской модели без username
    - Реализовал совместимость полей модели с 1ой версией api

--- bookexchange_project/users/models.py
+++ bookexchange_project/users/models.py
@@ -2,6 +2,15 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from users.managers import CustomUserManager
+
+# Перенести в константы books в следующей версии api
+EXCHANGE_CHOICES = (
+    ('PERSONAL', 'Личная встреча'),
+    ('PICKUP_POINT', 'Доставка в отделение или пункт выдачи'),
+    ('COURIER', 'Курьерская доставка'),
+)
+
 
 class CustomUser(AbstractUser):
 
@@ -16,9 +25,28 @@ class CustomUser(AbstractUser):
         'Город', max_length=settings.SHORT_FIELD_LENGTH, blank=True, null=True
     )
 
-    def __str__(self):
-        return self.email
+    # Удалить в следующей версии api
+    subway = models.CharField(
+        'Метро',
+        max_length=50,
+        blank=True,
+        null=True
+    )
+    exchange = models.CharField(
+        'Вариант обмена',
+        max_length=50,
+        choices=EXCHANGE_CHOICES,
+    )
+    # ------------------------------
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ()
 
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
