4ad5c3b0fbbccc5dc2bd4850aeb0018dd3543fa7
Стас Балджи <stack767@gmail.com>
2023-02-25 21:05:47 +0300

    Изменил настройки докер-компоуз для локальной разр

--- bookexchange/books/admin.py
+++ bookexchange/books/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from books.models import Advert, Author, Book, Favorites, Genre
+from books.models import Advert, Author, Book, BookCard, Favorites, Genre
 
 admin.site.register(Advert)
 admin.site.register(Book)
+admin.site.register(BookCard)
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(Favorites)
