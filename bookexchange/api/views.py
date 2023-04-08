f31b50c5c3e2c325cbca1295272af062273a0fad
Stanislav Chuprinskiy <stas.chuprinskiy@mail.ru>
2023-04-08 22:40:43 +0300

    Новый эндпойнт book_cards/id/favorite
    - Реализовано добавление объявления в избранное
    - Реализовано удаление объявления из избранного

--- bookexchange/api/views.py
+++ bookexchange/api/views.py
@@ -1,3 +1,9 @@
+from django.shortcuts import get_object_or_404
+from drf_yasg.utils import swagger_auto_schema, no_body
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from api.serializers import (
     AuthorSerializer,
     BookCardSerializer,
@@ -6,7 +12,6 @@ from api.serializers import (
     GenreSerializer,
 )
 from books.models import Author, Book, BookCard, Favorites, Genre
-from rest_framework import viewsets
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
@@ -22,6 +27,25 @@ class BookViewSet(viewsets.ModelViewSet):
 class BookCardViewSet(viewsets.ModelViewSet):
     queryset = BookCard.objects.all()
     serializer_class = BookCardSerializer
+
+    @swagger_auto_schema(request_body=no_body)
+    @action(detail=True, methods=["post"])
+    def favorite(self, request, pk=None):
+        instance = self.get_object()
+        user = request.user
+        _, _ = Favorites.objects.get_or_create(user=user, book_card=instance)
+        return Response(
+            self.get_serializer(instance=instance).data,
+            status=status.HTTP_201_CREATED,
+        )
+
+    @favorite.mapping.delete
+    def delete_favorite(self, request, *args, **kwargs):
+        instance = self.get_object()
+        user = request.user
+        favorite = get_object_or_404(Favorites, user=user, book_card=instance)
+        favorite.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class FavoritesViewSet(viewsets.ModelViewSet):
